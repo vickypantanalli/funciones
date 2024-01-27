c9608b059090b09102845c7f9cd701f8555c9e13
VickycPantanalli <victoriapantanalli@gmail.com>
2024-01-27 16:12:34 -0300

    cambio color a amarillo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
-function pintar(color = "green"){
-    ele.style.backgroundColor = 'yellow'
-    }
-    const ele = document.getElementById("ele1")
-    ele.addEventListener("click", pintar);
+const ele = document.getElementById("ele1")
 
+
+const pintar = (element, color = 'green') => {
+    element.style.backgroundColor = color
+}
+
+ele.addEventListener("click",()=>{
+        pintar (ele1, "yellow")
+    })
