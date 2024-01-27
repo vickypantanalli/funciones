3806c549fedec962c2169d9fa7a226690dd9bcb4
VickycPantanalli <victoriapantanalli@gmail.com>
2024-01-27 10:33:49 -0300

    set variables

---
+++ assets/js/4_colores.js
@@ -0,0 +1,26 @@
+let div1 = document.getElementById("div1")
+let div2 = document.getElementById("div2")
+let div3 = document.getElementById("div3")
+let div4 = document.getElementById("div4")
+
+let negro = (div1, color= "black") => {
+    div1.style.backgroundColor = color
+}
+
+
+
+
+
+/* const div1 = document.querySelector("#div1")
+div1.addEventListener("click", function(color){
+    div1.style.backgroundColor = "black"
+})
+
+/* negro = function(color) {
+    div1.backgroundColor = black
+} */
+
+negro = function(black) {
+    div1.style.backgroundColor = black
+
+} */
